ba43a9ccfeec62a02e12cd77ff9c84f92302d88e
Roman Letodiani💠 <roman.letodiani03@gmail.com>
2025-01-06 13:57:05 +0400

    env

--- env.ts
+++ env.ts
@@ -3,11 +3,9 @@ import zod from "zod";
 const envSchema = zod.object({
   AUTH_SECRET: zod.string().min(1).default("auth-secret"),
   AUTH_TRUST_HOST: zod.string().min(1).default("true"),
-
-  DATABASE_URL: zod
-    .string()
-    .min(1)
-    .default("postgresql://postgres:postgres@localhost:5432/nextjs-boilerplate"),
+  DATABASE_URL: zod.string().min(1),
+  AUTH_GOOGLE_ID: zod.string().min(1),
+  AUTH_GOOGLE_SECRET: zod.string().min(1),
 });
 
 export const env = envSchema.parse(process.env);
