4de50fee0027f32417aa7bcbed926e0c8d5a3dea
Roman Letodiani💠 <roman.letodiani03@gmail.com>
2025-01-07 01:16:16 +0400

    Refactor Login Page Component Structure
    - Simplified the SignIn component by removing session check and redirect logic, focusing solely on rendering the LoginPageWrapper, LoginForm, and LoginFeatures components.
    - Improved code readability and maintainability by streamlining the component structure.
    
    These changes enhance the organization of the login page and facilitate easier future modifications.

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,21 +1,12 @@
 import { LoginFeatures } from "@/components/(auth)/login/features/login-features";
 import { LoginForm } from "@/components/(auth)/login/form/login-form";
 import { LoginPageWrapper } from "@/components/(auth)/login/login-page-wrapper";
-import { auth } from "@/lib/auth";
-import { redirect } from "next/navigation";
 
-const SignIn = async () => {
-  const session = await auth();
-  if (session) {
-    redirect("/dashboard");
-  }
-
-  return (
-    <LoginPageWrapper>
-      <LoginForm />
-      <LoginFeatures />
-    </LoginPageWrapper>
-  );
-};
+const SignIn = async () => (
+  <LoginPageWrapper>
+    <LoginForm />
+    <LoginFeatures />
+  </LoginPageWrapper>
+);
 
 export default SignIn;
