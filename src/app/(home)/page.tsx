bafc3d00113418c1f15abbe9da592432b53bb231
Roman Letodiani💠 <roman.letodiani03@gmail.com>
2025-01-07 00:59:12 +0400

    rename home - (home) inside components

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,11 +1,11 @@
-import { CodePreview } from "@/components/home/views/code-preview";
-import { CTA } from "@/components/home/views/cta";
-import { Features, TechFeatures } from "@/components/home/views/features";
-import { HomeHero } from "@/components/home/views/hero";
-import { Performance } from "@/components/home/views/performance";
-import { Pricing } from "@/components/home/views/pricing";
-import { Stats } from "@/components/home/views/stats";
-import { Testimonials } from "@/components/home/views/testimonials";
+import { CodePreview } from "@/components/(home)/views/code-preview";
+import { CTA } from "@/components/(home)/views/cta";
+import { Features, TechFeatures } from "@/components/(home)/views/features";
+import { HomeHero } from "@/components/(home)/views/hero";
+import { Performance } from "@/components/(home)/views/performance";
+import { Pricing } from "@/components/(home)/views/pricing";
+import { Stats } from "@/components/(home)/views/stats";
+import { Testimonials } from "@/components/(home)/views/testimonials";
 
 const HomePage = async () => (
   <>
